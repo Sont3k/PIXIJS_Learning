65860a27540108b8b22d61707e2870762b8c0ace
Markus <markus.residence@gmail.com>
2019-11-18 18:19:00 +0200

    Done

--- main.js
+++ main.js
@@ -1,41 +1,74 @@
 import * as PIXI from "pixi.js";
 
-const app = new PIXI.Application({
-  width: 800,
-  height: 600,
-  backgroundColor: 0x1099bb,
-  resolution: window.devicePixelRatio || 1
-});
+const app = new PIXI.Application();
 document.body.appendChild(app.view);
 
-const container = new PIXI.Container();
+// holder to store bananas
+const bananas = [];
 
-app.stage.addChild(container);
+const totalBananas = 20;
 
-// Create a new texture
-const texture = PIXI.Texture.from('./assets/banan.png');
+for (let i = 0; i < totalBananas; i++) {
+  // create a new Sprite that uses the image name that we just generated as its source
+  const banana = PIXI.Sprite.from("./assets/banana_small.png");
 
-// Create a 5x5 grid of bunnies
-for(let i = 0; i < 25; i++) {
-    const banan = new PIXI.Sprite(texture);
-    banan.anchor.set(0.5);
-    banan.x = (i % 5) * 40;
-    banan.y = Math.floor(i / 5) * 40;
-    container.addChild(banan);
+  // set the anchor point so the texture is centerd on the sprite
+  banana.anchor.set(0.5);
+
+  // set a random scale for the banana - no point them all being the same size!
+  banana.scale.set(0.8 + Math.random() * 0.3);
+
+  // finally lets set the banana to be at a random position...
+  banana.x = Math.random() * app.screen.width;
+  banana.y = Math.random() * app.screen.height;
+
+  banana.tint = Math.random() * 0xffffff;
+
+  // create some extra properties that will control movement :
+  // create a random direction in radians. This is a number between 0 and PI*2 which is the equivalent of 0 - 360 degrees
+  banana.direction = Math.random() * Math.PI * 2;
+
+  // this number will be used to modify the direction of the banana over time
+  banana.turningSpeed = Math.random() - 0.8;
+
+  // create a random speed for the banana between 0 - 2
+  banana.speed = 2 + Math.random() * 2;
+
+  // finally we push the bananaa into the bananaas array so it it can be easily accessed later
+  bananas.push(banana);
+
+  app.stage.addChild(banana);
 }
 
-// Move container to the center
+// create a bounding box for the little dudes
+const bananaBoundsPadding = 100;
+const bananaBounds = new PIXI.Rectangle(
+  -bananaBoundsPadding,
+  -bananaBoundsPadding,
+  app.screen.width + bananaBoundsPadding * 2,
+  app.screen.height + bananaBoundsPadding * 2
+);
 
-container.x = app.screen.width / 2;
-container.y = app.screen.height / 2;
+app.ticker.add(() => {
+  // iterate through the bananas and update their position
+  for (let i = 0; i < bananas.length; i++) {
+    const banana = bananas[i];
+    banana.direction += banana.turningSpeed * 0.01;
+    banana.x += Math.sin(banana.direction) * banana.speed;
+    banana.y += Math.cos(banana.direction) * banana.speed;
+    banana.rotation = -banana.direction - Math.PI / 2;
 
-// Center banan sprite in local container coordinates
-container.pivot.x = container.width / 2;
-container.pivot.y = container.height / 2;
+    // wrap the bananas by testing their bounds...
+    if (banana.x < bananaBounds.x) {
+      banana.x += bananaBounds.width;
+    } else if (banana.x > bananaBounds.x + bananaBounds.width) {
+      banana.x -= bananaBounds.width;
+    }
 
-// Listen for animate update
-app.ticker.add((delta) => {
-    // rotate the container
-    // use delta to create frame-independent transform
-    container.rotation -= 0.01 * delta;
+    if (banana.y < bananaBounds.y) {
+      banana.y += bananaBounds.height;
+    } else if (banana.y > bananaBounds.y + bananaBounds.height) {
+      banana.y -= bananaBounds.height;
+    }
+  }
 });
