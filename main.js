bbaa92b367ba51604904c3b0d49393ecc7cb7732
Markus <markus.residence@gmail.com>
2019-11-18 20:08:19 +0200

    Fixed image name

--- main.js
+++ main.js
@@ -4,20 +4,20 @@ const app = new PIXI.Application({ transparent: true });
 document.body.appendChild(app.view);
 
 // create a new Sprite from an image path.
-const banan = PIXI.Sprite.from('./assets/banan.png');
+const banana = PIXI.Sprite.from('./assets/banana.png');
 
 // center the sprite's anchor point
-banan.anchor.set(0.5);
+banana.anchor.set(0.5);
 
 // move the sprite to the center of the screen
-banan.x = app.screen.width / 2;
-banan.y = app.screen.height / 2;
+banana.x = app.screen.width / 2;
+banana.y = app.screen.height / 2;
 
-app.stage.addChild(banan);
+app.stage.addChild(banana);
 
 app.ticker.add(() => {
     // just for fun, let's rotate mr rabbit a little
-    banan.rotation += 0.1;
+    banana.rotation += 0.1;
 });
 
 
