72a6e9e91043430cee9bc790304e6c7fb39c4858
Markus <markus.residence@gmail.com>
2019-11-18 17:32:56 +0200

    Created init code

--- main.js
+++ main.js
@@ -1,10 +1,41 @@
 import * as PIXI from "pixi.js";
 
-// The application will create a renderer using WebGL, if possible,
-// with a fallback to a canvas render. It will also setup the ticker
-// and the root stage PIXI.Container.
-const app = new PIXI.Application();
- 
-// The application will create a canvas element for you that you
-// can then insert into the DOM.
+const app = new PIXI.Application({
+  width: 800,
+  height: 600,
+  backgroundColor: 0x1099bb,
+  resolution: window.devicePixelRatio || 1
+});
 document.body.appendChild(app.view);
+
+const container = new PIXI.Container();
+
+app.stage.addChild(container);
+
+// Create a new texture
+const texture = PIXI.Texture.from('./assets/banan.png');
+
+// Create a 5x5 grid of bunnies
+for(let i = 0; i < 25; i++) {
+    const banan = new PIXI.Sprite(texture);
+    banan.anchor.set(0.5);
+    banan.x = (i % 5) * 40;
+    banan.y = Math.floor(i / 5) * 40;
+    container.addChild(banan);
+}
+
+// Move container to the center
+
+container.x = app.screen.width / 2;
+container.y = app.screen.height / 2;
+
+// Center banan sprite in local container coordinates
+container.pivot.x = container.width / 2;
+container.pivot.y = container.height / 2;
+
+// Listen for animate update
+app.ticker.add((delta) => {
+    // rotate the container
+    // use delta to create frame-independent transform
+    container.rotation -= 0.01 * delta;
+});
