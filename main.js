974676cf4119f825f6bddfa771dd9846bfa8918a
Markus <markus.residence@gmail.com>
2019-11-24 22:26:57 +0200

    Temp

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 import * as PIXI from "pixi.js";
 
-// The application will create a renderer using WebGL, if possible,
-// with a fallback to a canvas render. It will also setup the ticker
-// and the root stage PIXI.Container.
-const app = new PIXI.Application();
- 
-// The application will create a canvas element for you that you
-// can then insert into the DOM.
+const app = new PIXI.Application({
+  backgroundColor: 0x1099bb
+});
 document.body.appendChild(app.view);
+
+app.loader().add('');
